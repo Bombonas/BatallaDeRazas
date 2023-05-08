28f51b27e5d552ed001773b56400862f061fff3c
Pablo Mejías <116183883+Bombonas@users.noreply.github.com>
2023-05-08 20:18:16 +0200

    primer comit

---
+++ BatllaDeRazas/src/clases/Weapon.java
@@ -0,0 +1,71 @@
+package clases;
+
+public class Weapon {
+	private String name;
+	private int hp;
+	private int strength;
+	private int defense;
+	private int agility;
+	private int speed;
+	private int defeatPoints;
+	private int idWeapon;
+	private String url;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public int getStrength() {
+		return strength;
+	}
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	public int getDefense() {
+		return defense;
+	}
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+	public int getAgility() {
+		return agility;
+	}
+	public void setAgility(int agility) {
+		this.agility = agility;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getDefeatPoints() {
+		return defeatPoints;
+	}
+	public void setDefeatPoints(int defeatPoints) {
+		this.defeatPoints = defeatPoints;
+	}
+	public int getIdWeapon() {
+		return idWeapon;
+	}
+	public void setIdWeapon(int idWeapon) {
+		this.idWeapon = idWeapon;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
