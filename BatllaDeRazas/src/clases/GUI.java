c1731ddd510bc56702d2be8c6f31f43db1e55fa7
LeoAlendro171 <118751279+LeoAlendro171@users.noreply.github.com>
2023-05-09 20:56:25 +0200

    weapons added to weapons tab

--- BatllaDeRazas/src/clases/GUI.java
+++ BatllaDeRazas/src/clases/GUI.java
@@ -4,19 +4,25 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class GUI extends JFrame {
-    private JPanel mainPanel, tabsPanel, tabCharacters, tabWeapons, tabStage, tabRanking, characterPanel,
-            stagePanel, fightPanel;
+    private JPanel mainPanel, tabsPanel, tabWeapons, tabStage, tabRanking, characterPanel, fightPanel;
+    private EventPanel stagePanel, tabCharacters;
     private JButton button1;
-    private JLabel label1, labelStage1, labelStage2, labelStage3, labelWeapon1, labelWeapon2, labelWeapon3,
-            labelWeapon4, labelWeapon5, labelWeapon6, labelWeapon7, labelWeapon8, labelWeapon9;
+    private JLabel label1, labelCharacterPanel1, labelCharacterPanel2;
+    private JLabel[] labelStages, labelWeapons, labelCharacters;
     private JTabbedPane tabPane;
-    private BufferedImage background, stage1, stage2, stage3, weapon1, weapon2, weapon3, weapon4, weapon5,
-            weapon6, weapon7, weapon8, weapon9;
+    private BufferedImage[] stages, weapons, characters, idleAnim;
+    private int currentFrame = 0, numFrames = 5;
+    private Timer timer;
+    private ActionListener currentListener = null;
 
     public GUI() {
         //Define JFrame properties: size, close operation, title, location
@@ -31,45 +37,74 @@ public class GUI extends JFrame {
         mainPanel.setLayout(new BorderLayout());
         tabsPanel = new JPanel();
         tabsPanel.setLayout(new BoxLayout(tabsPanel, BoxLayout.Y_AXIS));
-        tabCharacters = new JPanel();
         tabWeapons = new JPanel();
         tabStage = new JPanel();
         tabRanking = new JPanel();
         characterPanel = new JPanel();
-        characterPanel.setBorder(new LineBorder(Color.GREEN, 1));//Border to see empty panel's location, delete later
-        stagePanel = new JPanel();
+        stagePanel = new EventPanel();
         stagePanel.setLayout(new BorderLayout());
         fightPanel = new JPanel();
         fightPanel.setBorder(new LineBorder(Color.BLUE, 1));//Border to see empty panel's location, delete later
 
-        //Define background variable with path to background.jpg
+        //Define image paths for stages, characters and weapons
         try {
-            //This has to be changed, placeholder
-            background = ImageIO.read(new File("BatllaDeRazas/src/background/Summer.jpg"));
+            stages = new BufferedImage[3];
+            stages[0] = ImageIO.read(new File("BatllaDeRazas/src/background/Summer.jpg"));
+            stages[1] = ImageIO.read(new File("BatllaDeRazas/src/background/desert.jpg"));
+            stages[2] = ImageIO.read(new File("BatllaDeRazas/src/background/winter.jpg"));
+            weapons = new BufferedImage[9];
+            weapons[0] = ImageIO.read(new File("BatllaDeRazas/src/weapons/dagger.png"));
+            weapons[1] = ImageIO.read(new File("BatllaDeRazas/src/weapons/sword.png"));
+            weapons[2] = ImageIO.read(new File("BatllaDeRazas/src/weapons/axe.png"));
+            weapons[3] = ImageIO.read(new File("BatllaDeRazas/src/weapons/dualsword.png"));
+            weapons[4] = ImageIO.read(new File("BatllaDeRazas/src/weapons/scimitar.png"));
+            weapons[5] = ImageIO.read(new File("BatllaDeRazas/src/weapons/bow.png"));
+            weapons[6] = ImageIO.read(new File("BatllaDeRazas/src/weapons/katana.png"));
+            weapons[7] = ImageIO.read(new File("BatllaDeRazas/src/weapons/stabby.png"));
+            weapons[8] = ImageIO.read(new File("BatllaDeRazas/src/weapons/dualaxe.png"));
+            characters = new BufferedImage[9];
+            characters[0] = ImageIO.read(new File("BatllaDeRazas/src/characters/dwarf1.png"));
+            characters[1] = ImageIO.read(new File("BatllaDeRazas/src/characters/human1.png"));
+            characters[2] = ImageIO.read(new File("BatllaDeRazas/src/characters/human1.png"));
+            characters[3] = ImageIO.read(new File("BatllaDeRazas/src/characters/dwarf1.png"));
+            characters[4] = ImageIO.read(new File("BatllaDeRazas/src/characters/dwarf1.png"));
+            characters[5] = ImageIO.read(new File("BatllaDeRazas/src/characters/dwarf1.png"));
+            characters[6] = ImageIO.read(new File("BatllaDeRazas/src/characters/dwarf1.png"));
+            characters[7] = ImageIO.read(new File("BatllaDeRazas/src/characters/dwarf1.png"));
+            characters[8] = ImageIO.read(new File("BatllaDeRazas/src/characters/dwarf1.png"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        //Set scaled instance of background on a JLabel and add to stage's JPanel
-        label1 = new JLabel(new ImageIcon(background.getScaledInstance(620, 590, BufferedImage.TYPE_INT_ARGB)));
+        //Initialize Character panel and prepare it for animations
+        tabCharacters = new EventPanel();
+        labelCharacters = new JLabel[9];
+        labelCharacters[0] = new JLabel();
+        for (int i = 0; i < labelCharacters.length; i++) {
+            labelCharacters[i] = new JLabel();
+            tabCharacters.add(labelCharacters[i]);
+        }
+        //Set a timer to update frames for the animations
+        timer = new Timer(100, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                currentFrame = (currentFrame + 1) % numFrames;
+                for (int i = 0; i < characters.length; i++) {
+                    BufferedImage subimage = characters[i].getSubimage(currentFrame*64, 0, 64, 32);
+                    labelCharacters[i].setIcon(new ImageIcon(subimage.getScaledInstance(200,100,
+                            BufferedImage.TYPE_INT_ARGB)));
+                    labelCharacters[i].addMouseListener(tabCharacters);
+                }
+            }
+        });
+        timer.start();
+
+        //Set default stage
+        label1 = new JLabel(new ImageIcon(stages[0].getScaledInstance(620, 590,
+                BufferedImage.TYPE_INT_ARGB)));
         stagePanel.add(label1);
 
-        //Define image paths for stages and weapons
-        try {
-            stage1 = ImageIO.read(new File("BatllaDeRazas/src/background/Summer.jpg"));
-            stage2 = ImageIO.read(new File("BatllaDeRazas/src/background/desert.jpg"));
-            stage3 = ImageIO.read(new File("BatllaDeRazas/src/background/winter.jpg"));
-            weapon1 = ImageIO.read(new File("BatllaDeRazas/src/weapons/dagger.png"));
-            weapon2 = ImageIO.read(new File("BatllaDeRazas/src/weapons/sword.png"));
-            weapon3 = ImageIO.read(new File("BatllaDeRazas/src/weapons/axe.png"));
-            weapon4 = ImageIO.read(new File("BatllaDeRazas/src/weapons/dualsword.png"));
-            weapon5 = ImageIO.read(new File("BatllaDeRazas/src/weapons/scimitar.png"));
-            weapon6 = ImageIO.read(new File("BatllaDeRazas/src/weapons/bow.png"));
-            weapon7 = ImageIO.read(new File("BatllaDeRazas/src/weapons/katana.png"));
-            weapon8 = ImageIO.read(new File("BatllaDeRazas/src/weapons/stabby.png"));
-            weapon9 = ImageIO.read(new File("BatllaDeRazas/src/weapons/dualaxe.png"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        //Label will include selected character
+        labelCharacterPanel1 = new JLabel();
+        characterPanel.add(labelCharacterPanel1);
 
         //Initialize JTabbedPane with tabs for character, weapon, stage and ranking, as well as size of the tab pane
         tabPane = new JTabbedPane();
@@ -89,47 +124,166 @@ public class GUI extends JFrame {
 
         //Fill stage tab with selectable stages
         tabStage.setLayout(new BoxLayout(tabStage, BoxLayout.Y_AXIS));
-        labelStage1 = new JLabel(new ImageIcon(stage1.getScaledInstance(700, 200, BufferedImage.TYPE_INT_ARGB)));
-        labelStage2 = new JLabel(new ImageIcon(stage2.getScaledInstance(700, 200, BufferedImage.TYPE_INT_ARGB)));
-        labelStage3 = new JLabel(new ImageIcon(stage3.getScaledInstance(700, 200, BufferedImage.TYPE_INT_ARGB)));
-        tabStage.add(labelStage1);
-        tabStage.add(labelStage2);
-        tabStage.add(labelStage3);
+        labelStages = new JLabel[3];
+        for (int i = 0; i < labelStages.length; i++) {
+            labelStages[i] = new JLabel(new ImageIcon(stages[i].getScaledInstance(700, 200,
+                    BufferedImage.TYPE_INT_ARGB)));
+            labelStages[i].addMouseListener(stagePanel);
+            tabStage.add(labelStages[i]);
+        }
 
         //Fill weapons tab with selectable weapons
         tabWeapons.setLayout(new GridLayout(3,3));
-        labelWeapon1 = new JLabel(new ImageIcon(weapon1.getScaledInstance(200,200,
-                BufferedImage.TYPE_INT_ARGB)));
-        labelWeapon2 = new JLabel(new ImageIcon(weapon2.getScaledInstance(200,200,
-                BufferedImage.TYPE_INT_ARGB)));
-        labelWeapon3 = new JLabel(new ImageIcon((weapon3.getScaledInstance(200, 200,
-                BufferedImage.TYPE_INT_ARGB))));
-        labelWeapon4 = new JLabel(new ImageIcon(weapon4.getScaledInstance(200, 200,
-                BufferedImage.TYPE_INT_ARGB)));
-        labelWeapon5 = new JLabel(new ImageIcon(weapon5.getScaledInstance(200, 200,
-                BufferedImage.TYPE_INT_ARGB)));
-        labelWeapon6 = new JLabel(new ImageIcon(weapon6.getScaledInstance(200, 200,
-                BufferedImage.TYPE_INT_ARGB)));
-        labelWeapon7 = new JLabel(new ImageIcon(weapon7.getScaledInstance(200, 200,
-                BufferedImage.TYPE_INT_ARGB)));
-        labelWeapon8 = new JLabel(new ImageIcon(weapon8.getScaledInstance(200, 200,
-                BufferedImage.TYPE_INT_ARGB)));
-        labelWeapon9 = new JLabel(new ImageIcon(weapon9.getScaledInstance(200, 200,
-                BufferedImage.TYPE_INT_ARGB)));
+        labelWeapons = new JLabel[9];
+        for (int i = 0; i < labelWeapons.length; i++) {
+            labelWeapons[i] = new JLabel(new ImageIcon(weapons[i].getScaledInstance(200, 200,
+                    BufferedImage.TYPE_INT_ARGB)));
+            tabWeapons.add(labelWeapons[i]);
+        }
 
-        tabWeapons.add(labelWeapon1);
-        tabWeapons.add(labelWeapon2);
-        tabWeapons.add(labelWeapon3);
-        tabWeapons.add(labelWeapon4);
-        tabWeapons.add(labelWeapon5);
-        tabWeapons.add(labelWeapon6);
-        tabWeapons.add(labelWeapon7);
-        tabWeapons.add(labelWeapon8);
-        tabWeapons.add(labelWeapon9);
 
 
         //Add main panel to JFrame and set visible
         add(mainPanel);
         setVisible(true);
+    }
+
+    class EventPanel extends JPanel implements MouseListener {
+        public void mouseClicked(MouseEvent e) {
+            //Change to stage 1
+            if (e.getSource().equals(labelStages[0])) {
+                label1.setIcon(new ImageIcon(stages[0].getScaledInstance(620, 590,
+                        BufferedImage.TYPE_INT_ARGB)));
+            }
+            //Change to stage 2
+            else if (e.getSource().equals(labelStages[1])) {
+                label1.setIcon(new ImageIcon(stages[1].getScaledInstance(620, 590,
+                        BufferedImage.TYPE_INT_ARGB)));
+            }
+            //Change to stage 3
+            else if (e.getSource().equals(labelStages[2])) {
+                label1.setIcon(new ImageIcon(stages[2].getScaledInstance(620, 590,
+                        BufferedImage.TYPE_INT_ARGB)));
+            }
+            if (currentListener != null) {
+                timer.removeActionListener(currentListener);
+            }
+            //Change selected character
+            if (e.getSource().equals(labelCharacters[0])) {
+                currentListener = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        currentFrame = (currentFrame + 1) % numFrames;
+                        BufferedImage subimage = characters[0].getSubimage(currentFrame*64, 0, 64, 32);
+                        labelCharacterPanel1.setIcon(new ImageIcon(subimage.getScaledInstance(200,100,
+                                BufferedImage.TYPE_INT_ARGB)));
+                    }
+                };
+                timer.addActionListener(currentListener);
+            }
+            else if (e.getSource().equals(labelCharacters[1])) {
+                currentListener = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        currentFrame = (currentFrame + 1) % numFrames;
+                        BufferedImage subimage = characters[1].getSubimage(currentFrame*64, 0, 64, 32);
+                        labelCharacterPanel1.setIcon(new ImageIcon(subimage.getScaledInstance(200,100,
+                                BufferedImage.TYPE_INT_ARGB)));
+                    }
+                };
+                timer.addActionListener(currentListener);
+            }
+            else if (e.getSource().equals(labelCharacters[2])) {
+                currentListener = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        currentFrame = (currentFrame + 1) % numFrames;
+                        BufferedImage subimage = characters[2].getSubimage(currentFrame*64, 0, 64, 32);
+                        labelCharacterPanel1.setIcon(new ImageIcon(subimage.getScaledInstance(200,100,
+                                BufferedImage.TYPE_INT_ARGB)));
+                    }
+                };
+                timer.addActionListener(currentListener);
+            }
+            else if (e.getSource().equals(labelCharacters[3])) {
+                currentListener = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        currentFrame = (currentFrame + 1) % numFrames;
+                        BufferedImage subimage = characters[3].getSubimage(currentFrame*64, 0, 64, 32);
+                        labelCharacterPanel1.setIcon(new ImageIcon(subimage.getScaledInstance(200,100,
+                                BufferedImage.TYPE_INT_ARGB)));
+                    }
+                };
+                timer.addActionListener(currentListener);
+            }
+            else if (e.getSource().equals(labelCharacters[4])) {
+                currentListener = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        currentFrame = (currentFrame + 1) % numFrames;
+                        BufferedImage subimage = characters[4].getSubimage(currentFrame*64, 0, 64, 32);
+                        labelCharacterPanel1.setIcon(new ImageIcon(subimage.getScaledInstance(200,100,
+                                BufferedImage.TYPE_INT_ARGB)));
+                    }
+                };
+                timer.addActionListener(currentListener);
+            }
+            else if (e.getSource().equals(labelCharacters[5])) {
+                currentListener = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        currentFrame = (currentFrame + 1) % numFrames;
+                        BufferedImage subimage = characters[5].getSubimage(currentFrame*64, 0, 64, 32);
+                        labelCharacterPanel1.setIcon(new ImageIcon(subimage.getScaledInstance(200,100,
+                                BufferedImage.TYPE_INT_ARGB)));
+                    }
+                };
+                timer.addActionListener(currentListener);
+            }
+            else if (e.getSource().equals(labelCharacters[6])) {
+                currentListener = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        currentFrame = (currentFrame + 1) % numFrames;
+                        BufferedImage subimage = characters[6].getSubimage(currentFrame*64, 0, 64, 32);
+                        labelCharacterPanel1.setIcon(new ImageIcon(subimage.getScaledInstance(200,100,
+                                BufferedImage.TYPE_INT_ARGB)));
+                    }
+                };
+                timer.addActionListener(currentListener);
+            }
+            else if (e.getSource().equals(labelCharacters[7])) {
+                currentListener = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        currentFrame = (currentFrame + 1) % numFrames;
+                        BufferedImage subimage = characters[7].getSubimage(currentFrame*64, 0, 64, 32);
+                        labelCharacterPanel1.setIcon(new ImageIcon(subimage.getScaledInstance(200,100,
+                                BufferedImage.TYPE_INT_ARGB)));
+                    }
+                };
+                timer.addActionListener(currentListener);
+            }
+            else if (e.getSource().equals(labelCharacters[8])) {
+                currentListener = new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        currentFrame = (currentFrame + 1) % numFrames;
+                        BufferedImage subimage = characters[8].getSubimage(currentFrame*64, 0, 64, 32);
+                        labelCharacterPanel1.setIcon(new ImageIcon(subimage.getScaledInstance(200,100,
+                                BufferedImage.TYPE_INT_ARGB)));
+                    }
+                };
+                timer.addActionListener(currentListener);
+            }
+        }
+        public void mousePressed(MouseEvent e) {}
+
+        public void mouseReleased(MouseEvent e) {}
+
+        public void mouseEntered(MouseEvent e) {}
+
+        public void mouseExited(MouseEvent e) {}
     }
 }
