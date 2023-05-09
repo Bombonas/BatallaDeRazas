5153a0647f860bdb683131f44464195a9b4cad70
LeoAlendro171 <118751279+LeoAlendro171@users.noreply.github.com>
2023-05-09 15:51:26 +0200

    Revert "Revert "holaaaa""
    This reverts commit d667241f20e888f0b874acc7415a371ddcd2c8ce.

---
+++ BatllaDeRazas/src/clases/WarriorContainer.java
@@ -0,0 +1,14 @@
+package clases;
+
+import java.util.ArrayList;
+
+public class WarriorContainer {
+	private ArrayList<Warrior> warriors;
+
+	
+	public ArrayList<Warrior> getWarriors() {
+		return warriors;
+	}
+	
+
+}
