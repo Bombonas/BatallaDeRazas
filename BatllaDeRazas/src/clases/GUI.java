49d28d507a72a13f3b31404864ae3ecf4a469a29
LeoAlendro171 <118751279+LeoAlendro171@users.noreply.github.com>
2023-05-09 17:31:19 +0200

    weapons added to weapons tab

--- BatllaDeRazas/src/clases/GUI.java
+++ BatllaDeRazas/src/clases/GUI.java
@@ -10,11 +10,13 @@ import java.io.IOException;
 
 public class GUI extends JFrame {
     private JPanel mainPanel, tabsPanel, tabCharacters, tabWeapons, tabStage, tabRanking, characterPanel,
-    stagePanel, fightPanel;
+            stagePanel, fightPanel;
     private JButton button1;
-    private JLabel label1;
+    private JLabel label1, labelStage1, labelStage2, labelStage3, labelWeapon1, labelWeapon2, labelWeapon3,
+            labelWeapon4, labelWeapon5, labelWeapon6, labelWeapon7, labelWeapon8, labelWeapon9;
     private JTabbedPane tabPane;
-    private BufferedImage background;
+    private BufferedImage background, stage1, stage2, stage3, weapon1, weapon2, weapon3, weapon4, weapon5,
+            weapon6, weapon7, weapon8, weapon9;
 
     public GUI() {
         //Define JFrame properties: size, close operation, title, location
@@ -42,6 +44,7 @@ public class GUI extends JFrame {
 
         //Define background variable with path to background.jpg
         try {
+            //This has to be changed, placeholder
             background = ImageIO.read(new File("BatllaDeRazas/src/background/Summer.jpg"));
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -49,6 +52,24 @@ public class GUI extends JFrame {
         //Set scaled instance of background on a JLabel and add to stage's JPanel
         label1 = new JLabel(new ImageIcon(background.getScaledInstance(620, 590, BufferedImage.TYPE_INT_ARGB)));
         stagePanel.add(label1);
+
+        //Define image paths for stages and weapons
+        try {
+            stage1 = ImageIO.read(new File("BatllaDeRazas/src/background/Summer.jpg"));
+            stage2 = ImageIO.read(new File("BatllaDeRazas/src/background/desert.jpg"));
+            stage3 = ImageIO.read(new File("BatllaDeRazas/src/background/winter.jpg"));
+            weapon1 = ImageIO.read(new File("BatllaDeRazas/src/weapons/dagger.png"));
+            weapon2 = ImageIO.read(new File("BatllaDeRazas/src/weapons/sword.png"));
+            weapon3 = ImageIO.read(new File("BatllaDeRazas/src/weapons/axe.png"));
+            weapon4 = ImageIO.read(new File("BatllaDeRazas/src/weapons/dualsword.png"));
+            weapon5 = ImageIO.read(new File("BatllaDeRazas/src/weapons/scimitar.png"));
+            weapon6 = ImageIO.read(new File("BatllaDeRazas/src/weapons/bow.png"));
+            weapon7 = ImageIO.read(new File("BatllaDeRazas/src/weapons/katana.png"));
+            weapon8 = ImageIO.read(new File("BatllaDeRazas/src/weapons/stabby.png"));
+            weapon9 = ImageIO.read(new File("BatllaDeRazas/src/weapons/dualaxe.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         //Initialize JTabbedPane with tabs for character, weapon, stage and ranking, as well as size of the tab pane
         tabPane = new JTabbedPane();
@@ -65,6 +86,46 @@ public class GUI extends JFrame {
         stagePanel.add(fightPanel, BorderLayout.PAGE_END);
         mainPanel.add(tabsPanel, BorderLayout.WEST);
         mainPanel.add(stagePanel, BorderLayout.CENTER);
+
+        //Fill stage tab with selectable stages
+        tabStage.setLayout(new BoxLayout(tabStage, BoxLayout.Y_AXIS));
+        labelStage1 = new JLabel(new ImageIcon(stage1.getScaledInstance(700, 200, BufferedImage.TYPE_INT_ARGB)));
+        labelStage2 = new JLabel(new ImageIcon(stage2.getScaledInstance(700, 200, BufferedImage.TYPE_INT_ARGB)));
+        labelStage3 = new JLabel(new ImageIcon(stage3.getScaledInstance(700, 200, BufferedImage.TYPE_INT_ARGB)));
+        tabStage.add(labelStage1);
+        tabStage.add(labelStage2);
+        tabStage.add(labelStage3);
+
+        //Fill weapons tab with selectable weapons
+        tabWeapons.setLayout(new GridLayout(3,3));
+        labelWeapon1 = new JLabel(new ImageIcon(weapon1.getScaledInstance(200,200,
+                BufferedImage.TYPE_INT_ARGB)));
+        labelWeapon2 = new JLabel(new ImageIcon(weapon2.getScaledInstance(200,200,
+                BufferedImage.TYPE_INT_ARGB)));
+        labelWeapon3 = new JLabel(new ImageIcon((weapon3.getScaledInstance(200, 200,
+                BufferedImage.TYPE_INT_ARGB))));
+        labelWeapon4 = new JLabel(new ImageIcon(weapon4.getScaledInstance(200, 200,
+                BufferedImage.TYPE_INT_ARGB)));
+        labelWeapon5 = new JLabel(new ImageIcon(weapon5.getScaledInstance(200, 200,
+                BufferedImage.TYPE_INT_ARGB)));
+        labelWeapon6 = new JLabel(new ImageIcon(weapon6.getScaledInstance(200, 200,
+                BufferedImage.TYPE_INT_ARGB)));
+        labelWeapon7 = new JLabel(new ImageIcon(weapon7.getScaledInstance(200, 200,
+                BufferedImage.TYPE_INT_ARGB)));
+        labelWeapon8 = new JLabel(new ImageIcon(weapon8.getScaledInstance(200, 200,
+                BufferedImage.TYPE_INT_ARGB)));
+        labelWeapon9 = new JLabel(new ImageIcon(weapon9.getScaledInstance(200, 200,
+                BufferedImage.TYPE_INT_ARGB)));
+
+        tabWeapons.add(labelWeapon1);
+        tabWeapons.add(labelWeapon2);
+        tabWeapons.add(labelWeapon3);
+        tabWeapons.add(labelWeapon4);
+        tabWeapons.add(labelWeapon5);
+        tabWeapons.add(labelWeapon6);
+        tabWeapons.add(labelWeapon7);
+        tabWeapons.add(labelWeapon8);
+        tabWeapons.add(labelWeapon9);
 
 
         //Add main panel to JFrame and set visible
