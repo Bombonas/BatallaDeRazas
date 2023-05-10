67ed6d9b6cb97a524bbc204c745722ccf70f9d87
MatillaSan <dsanchezmatilla.cf@iesesteveterradas.cat>
2023-05-10 23:47:17 +0200

    Creada Clase de Conexiones a DB

--- BatallaDeRazas/src/clases/WarriorContainer.java
+++ BatallaDeRazas/src/clases/WarriorContainer.java
@@ -17,10 +17,6 @@ public class WarriorContainer {
 
 		warriors = new ArrayList<Warrior>();
 
-		String url = "jdbc:mysql://localhost/raceWar?serverTimezone=UTC";
-		String usr = "root";
-		String pas = "1234";
-
 		// ArrayList with all the weapons
 		ArrayList<Weapon> weaponList = new ArrayList<Weapon>();
 		for (Weapon w : new WeaponContainer().getWeapons()) {
@@ -31,26 +27,17 @@ public class WarriorContainer {
 		ArrayList<Weapon> weaponAvailable = new ArrayList<Weapon>();
 
 		try {
-			// Start driver
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("Driver charged successfully");
-
-			// Create DB connection
-			Connection conn = DriverManager.getConnection(url, usr, pas);
-			System.out.println("Connection created successfully");
-
-			// Instance the statements objects with the connections
-			Statement stmnt1 = conn.createStatement(); // This statement is used to call the warriors query
-			Statement stmnt2 = conn.createStatement(); // This Statement is used to call the weapon-available query
+			DataBaseConn conn1 = new DataBaseConn(); // This connection is used to call the warriors query
+			DataBaseConn conn2 = new DataBaseConn(); // This connection is used to call the weaponAvailable query
 
 			// This Result Set call the query that have all the values for the Warriors class
-			ResultSet rs1 = stmnt1.executeQuery("SELECT w.playable, r.race, w.hp, w.strength, w.defense, " +
+			ResultSet rs1 = conn1.getQueryRS("SELECT w.playable, r.race, w.hp, w.strength, w.defense, " +
 												"w.agility, w.speed, w.image_path, w.defeatPoints, w.id, " +
 												"w.name, w.race_id\n" +
 												"FROM warriors w\n" +
 												"INNER JOIN races r on w.race_id = r.id;");
 
-			int race = 0; // int keep the actual warrior race
+			int race; // int keep the actual warrior race
 
 			int position = 0; // int to know the position in ArrayList warriors
 
@@ -62,7 +49,7 @@ public class WarriorContainer {
 				weaponAvailable.clear();
 
 				// Create the query to match warrior with his weapons
-				ResultSet rs2 = stmnt2.executeQuery("SELECT * FROM weapon_available WHERE race_id = " + race);
+				ResultSet rs2 = conn2.getQueryRS("SELECT * FROM weapon_available WHERE race_id = " + race);
 				// Iterator used to fill the ArrayList weaponAvailable
 				while (rs2.next()) {
 					weaponAvailable.add(weaponList.get(rs2.getInt(1) - 1));
@@ -79,10 +66,11 @@ public class WarriorContainer {
 				position ++;
 			}
 
-		} catch (ClassNotFoundException e) {
-			System.out.println("Driver not charged successfully");
+			conn1.closeConn();
+			conn2.closeConn();
+
 		} catch (SQLException e) {
-			System.out.println("Connection not created successfully");
+			System.out.println("Error executing ResultSet");
 		}
 
 	}
