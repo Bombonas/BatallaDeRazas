1e1659e6ce4ac3a38479cb9c9bd94bf7256688b0
LeoAlendro171 <118751279+LeoAlendro171@users.noreply.github.com>
2023-05-12 20:05:18 +0200

    holi

--- BatallaDeRazas/src/clases/GUI.java
+++ BatallaDeRazas/src/clases/GUI.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -17,7 +18,7 @@ public class GUI extends JFrame {
     private JPanel mainPanel, tabsPanel, tabStage, tabRanking, characterPanel, fightPanel;
     private EventPanel stagePanel, tabCharacters, tabWeapons;
     private JButton fightButton;
-    private JLabel label1, labelCharacterPanel, labelSelectedWeapon, labelWeaponBackground;
+    private JLabel label1, labelCharacterPanel, labelSelectedWeapon, labelCPUwarrior, labelCPUWeapon;
     private JLabel[] labelStages, labelCharacters, weaponLabel;
     private JTabbedPane tabPane;
     private BufferedImage[] stages, characters;
@@ -65,7 +66,29 @@ public class GUI extends JFrame {
         tabWeapons.addMouseListener(tabWeapons);
         tabStage = new JPanel();
         tabRanking = new JPanel();
-        characterPanel = new JPanel();
+        characterPanel = new JPanel() {
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                try {
+                    BufferedImage characterPanelBackground = ImageIO.read(new File(
+                            "BatallaDeRazas/src/background/charactersSelectedBackground.jpg"));
+                    Image scaledBackground = characterPanelBackground.getScaledInstance(650,
+                            100,Image.SCALE_SMOOTH);
+                    g.drawImage(scaledBackground, 0, 0, this);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                g2d.setColor(new Color(185, 61, 61));
+                String text = "YOU";
+                String text2 = "CPU";
+                g2d.setFont(pixelFont);
+                g2d.drawString(text, 80, 70);
+                g2d.drawString(text2, 480, 70);
+            }
+        };
+        System.out.println(characterPanel.getWidth());
+        System.out.println(characterPanel.getHeight());
         characterPanel.setPreferredSize(new Dimension(300, 100));
         stagePanel = new EventPanel();
         stagePanel.setLayout(new BorderLayout());
@@ -139,10 +162,14 @@ public class GUI extends JFrame {
         labelCharacters = new JLabel[9];
         //Label will include selected character
         labelCharacterPanel = new JLabel();
+        labelCPUwarrior = new JLabel();
         labelSelectedWeapon = new JLabel();
+        labelCPUWeapon = new JLabel();
         characterPanel.setLayout(new BoxLayout(characterPanel, BoxLayout.X_AXIS));
-        characterPanel.add(labelCharacterPanel);
         characterPanel.add(labelSelectedWeapon);
+        characterPanel.add(labelCharacterPanel);
+        characterPanel.add(labelCPUwarrior);
+        characterPanel.add(labelCPUWeapon);
         //Fill characters panel with empty labels that will be replaced with animated characters images
         for (int i = 0; i < labelCharacters.length; i++) {
             labelCharacters[i] = new JLabel();
@@ -160,10 +187,27 @@ public class GUI extends JFrame {
                         if (i > 5) {
                             //Lower height value for dwarf characters
                             labelCharacterPanel.setIcon(new ImageIcon(subimage.getScaledInstance(250, 100,
-                                    BufferedImage.TYPE_INT_ARGB)));
+                                    Image.SCALE_SMOOTH)));
                         }else {
                             labelCharacterPanel.setIcon(new ImageIcon(subimage.getScaledInstance(250, 250,
-                                    BufferedImage.TYPE_INT_ARGB)));
+                                    Image.SCALE_SMOOTH)));
+                        }
+                    }else if (ch.getName().equals(cpu.getWarrior().getName())) {
+                        BufferedImage cpuFlip = new BufferedImage(subimage.getWidth(), subimage.getHeight(),
+                                subimage.getType());
+                        Graphics2D g2d = cpuFlip.createGraphics();
+                        AffineTransform flipImage = AffineTransform.getScaleInstance(-1, 1);
+                        flipImage.translate(-subimage.getWidth(), 0);
+                        g2d.transform(flipImage);
+                        g2d.drawImage(subimage, 0, 0, null);
+                        g2d.dispose();
+                        if (i > 5) {
+                            //Lower height value for dwarf characters
+                            labelCPUwarrior.setIcon(new ImageIcon(cpuFlip.getScaledInstance(250, 100,
+                                    Image.SCALE_SMOOTH)));
+                        }else {
+                            labelCPUwarrior.setIcon(new ImageIcon(cpuFlip.getScaledInstance(250, 250,
+                                    Image.SCALE_SMOOTH)));
                         }
                     }
                     //Fill character tab with every available character
@@ -279,20 +323,25 @@ public class GUI extends JFrame {
     }
     public void setSelectedWeaponImage() {
         BufferedImage selectedWeapon;
+        BufferedImage cpuWeapon;
         if (usr.getWeapon() == null) {
             try {
                 selectedWeapon = ImageIO.read(new File("BatallaDeRazas/src/weapons/noweapon.png"));
+                cpuWeapon = ImageIO.read(new File(cpu.getWeapon().getUrl()));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }else{
             try {
                 selectedWeapon = ImageIO.read(new File(usr.getWeapon().getUrl()));
+                cpuWeapon = ImageIO.read(new File(cpu.getWeapon().getUrl()));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
         labelSelectedWeapon.setIcon(new ImageIcon(selectedWeapon.getScaledInstance(70, 70,
+                BufferedImage.TYPE_INT_ARGB)));
+        labelCPUWeapon.setIcon(new ImageIcon(cpuWeapon.getScaledInstance(70, 70,
                 BufferedImage.TYPE_INT_ARGB)));
     }
     //Method to return true value if the player's current warrior can use the player's current weapon
